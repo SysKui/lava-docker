274876a1911be10e3dafe6dca8c5e1f8c607962f
Yang Yexuan <1182282462@qq.com>
2025-02-24 11:20:03 +0800

    Extract the kernel config and check if kernel supports pvpanic

--- re-deploy.sh
+++ re-deploy.sh
@@ -4,8 +4,9 @@ if [ "$(id -u)" -ne 0 ]; then
   echo "should be root"
   exit 1
 fi
+./backup.sh
 systemctl stop lava
 rm -r output/
 ./lavalab-gen.sh
-cp ./backup-20241119_1241/* ./output/lava-test-1/master/backup && cd output/lava-test-1/ && docker-compose build
+cp ./backup-latest/* ./output/lava-test-1/master/backup && cd output/lava-test-1/ && docker-compose build
 systemctl start lava
